20cea91d5a2335b4132df6e684c3c53b055bcb44
William Jensen <williamjensenbc@gmail.com>
2021-11-06 00:07:26 +0100

    Faster transformation mat4 function A faster implementation of the mat4 function

--- 3D_1/phm_object.h
+++ 3D_1/phm_object.h
@@ -15,15 +15,32 @@ struct Transform
 
 	inline glm::mat4 mat4() const
 	{ 
-		auto transform = glm::translate(glm::mat4{ 1.0f }, translation);
-		
-		transform = glm::rotate(transform, rotation.y, { 0.0f, 1.0f, 0.0f });
-		transform = glm::rotate(transform, rotation.x, { 1.0f, 0.0f, 0.0f });
-		transform = glm::rotate(transform, rotation.z, { 0.0f, 0.0f, 1.0f });
-
-		transform = glm::scale(transform, scale);
-		
-		return transform;
+        const float c3 = glm::cos(rotation.z);
+        const float s3 = glm::sin(rotation.z);
+        const float c2 = glm::cos(rotation.x);
+        const float s2 = glm::sin(rotation.x);
+        const float c1 = glm::cos(rotation.y);
+        const float s1 = glm::sin(rotation.y);
+        return glm::mat4{
+            {
+                scale.x * (c1 * c3 + s1 * s2 * s3),
+                scale.x * (c2 * s3),
+                scale.x * (c1 * s2 * s3 - c3 * s1),
+                0.0f,
+            },
+            {
+                scale.y * (c3 * s1 * s2 - c1 * s3),
+                scale.y * (c2 * c3),
+                scale.y * (c1 * c3 * s2 + s1 * s3),
+                0.0f,
+            },
+            {
+                scale.z * (c2 * s1),
+                scale.z * (-s2),
+                scale.z * (c1 * c2),
+                0.0f,
+            },
+            {translation.x, translation.y, translation.z, 1.0f} };
 	};
 
 	glm::vec3 rotation{};
